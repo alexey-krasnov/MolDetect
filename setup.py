e84b4c9a522367decff0caaa8ae7f9b052297e6f
Dr. Aleksei Krasnov <dr.aleksei.krasnov@gmail.com>
2025-12-30 19:01:49 +0100

    chore: fixed MolScribe as requirement. Added return_dict_in_generate=True to GenerationConfig

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 from distutils.core import setup
 from pathlib import Path
 
-
-# Use importlib.metadata instead of pkg_resources (which is deprecated)
-try:
-    from importlib.metadata import distribution, PackageNotFoundError
-except ImportError:
-    # For Python < 3.8
-    from importlib_metadata import distribution, PackageNotFoundError
-
-
 def get_install_requires():
     """Returns requirements.txt parsed to a list"""
     fname = Path(__file__).parent / 'requirements.txt'
@@ -19,21 +10,7 @@ def get_install_requires():
             targets = f.read().splitlines()
     return targets
 
-
-def is_package_installed(package_name):
-    """Check if a package is installed using importlib.metadata"""
-    try:
-        distribution(package_name)
-        return True
-    except PackageNotFoundError:
-        return False
-
-
 requirements = get_install_requires()
-
-# Check if MolScribe is installed, if not add it to dependencies
-if not is_package_installed('MolScribe'):
-    requirements.append('MolScribe @ git+https://github.com/alexey-krasnov/MolScribe')
 
 setup(name='RxnScribe',
       version='1.0',
